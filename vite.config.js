ba3d5c23e57a5f4ef57f85f3cc8991b67afa995b
zhoupanpan <601460569@qq.com>
2025-12-19 11:33:49 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  root: '.',
+  root: process.cwd(),
   plugins: [
     vue(),
     VitePWA({
