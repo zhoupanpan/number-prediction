d24952457478ac75ad15d842c184831a6ba1b810
zhoupanpan <601460569@qq.com>
2025-12-23 15:56:23 +0800

    update git pages

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/number-prediction/sw.js', { scope: '/number-prediction/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
